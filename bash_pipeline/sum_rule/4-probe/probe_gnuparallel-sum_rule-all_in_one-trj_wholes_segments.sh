65355ab8d1869eb28c7ad0b7e8b91a96b6017495
amirhsi <54184877+amirhs1@users.noreply.github.com>
2022-07-17 22:37:41 -0400

    modified by OnDrive

---
+++ bash_pipeline/sum_rule/4-probe/probe_gnuparallel-sum_rule-all_in_one-trj_wholes_segments.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# copies the python script asnd package which are then executed by gnuparallal.
+for dir in N*/; do
+    echo "$dir"
+    cp probe-sum_rule-all_in_one-trj_wholes_segments*.py probe-sum_rule-all_in_one-trj_wholes_segments.py
+    mv probe-sum_rule-all_in_one-trj_wholes_segments.py "$dir"
+    cp -R polyphys "${dir}"
+done
