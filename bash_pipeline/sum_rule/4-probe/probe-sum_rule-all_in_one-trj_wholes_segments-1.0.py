d7cbb4155c10cfec1ba9277ae0b6ebef55d92515
amirhsi <54184877+amirhs1@users.noreply.github.com>
2022-07-17 23:04:19 -0400

    sum_rule probe scripts

--- bash_pipeline/sum_rule/4-probe/probe-sum_rule-all_in_one-trj_wholes_segments-1.0.py
+++ bash_pipeline/sum_rule/4-probe/probe-sum_rule-all_in_one-trj_wholes_segments-1.0.py
@@ -4,44 +4,26 @@ from polyphys.probe import prober
 from polyphys.manage.parser import SumRule
 
 
+# analyzing bug files.
 geometry = 'biaxial'
 group = 'bug'
-topo_lineage = 'whole'
-lineage = 'segment'
+lineage = 'whole'
 save_to = './'
-bug_trjs = glob('./N*' + group + '*')
-bug_trjs = organizer.sort_filenames(
-    bug_trjs,
-    fmts=['.' + group + '.lammpstrj']
+bug_pairs = glob('./N*' + group + '*')
+bug_pairs = organizer.sort_filenames(
+    bug_pairs,
+    fmts=['.' + group + '.data', '.' + group + '.lammpstrj']
 )
-bug_trjs = [bug_trj[0] for bug_trj in bug_trjs]
-bug_topo = glob('./N*' + group + '*')
-bug_topo = organizer.sort_filenames(bug_topo, fmts=['.' + group + '.data'])
-bug_topo = bug_topo[0][0]
-max_segment_id = len(bug_trjs)
-# analyzig all files
-# it is assumed that the all trjs are numbers from 1 to max_segment_id
-for bug_trj in bug_trjs[:1]:
-    trj_info = SumRule(
+for (bug_topo, bug_trj) in bug_pairs:
+    prober.sum_rule_bug(
+        bug_topo,
         bug_trj,
-        geometry=geometry,
-        group=group,
-        lineage=topo_lineage
+        geometry,
+        lineage,
+        continuous=False,
+        save_to=save_to
     )
-    # all the frames in the last segment are probed:
-    if trj_info.segment_id == max_segment_id:
-        prober.sum_rule_bug(
-            bug_topo, bug_trj, geometry, lineage, save_to=save_to
-        )
-    else:
-        prober.sum_rule_bug(
-            bug_topo,
-            bug_trj,
-            geometry,
-            lineage,
-            save_to=save_to,
-            continuous=True
-        )
+
 
 geometry = 'biaxial'
 group = 'all'
