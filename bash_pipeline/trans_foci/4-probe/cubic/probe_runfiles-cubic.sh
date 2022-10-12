befd0b0278f9f64e45949f0d68b576cb92f07376
amirhsi <54184877+amirhs1@users.noreply.github.com>
2022-10-11 23:14:24 -0400

    some typos fixed

--- bash_pipeline/trans_foci/4-probe/cubic/probe_runfiles-cubic.sh
+++ bash_pipeline/trans_foci/4-probe/cubic/probe_runfiles-cubic.sh
@@ -4,6 +4,6 @@ cp ../PolyPhys/bash_pipeline/trans_foci/4-probe/cubic/sbatch*.sh .
 cp ../PolyPhys/bash_pipeline/trans_foci/4-probe/cubic/submit*.sh .
 cp ../PolyPhys/bash_pipeline/trans_foci/4-probe/cubic/probe-*.py .
 cp ../PolyPhys/bash_pipeline/trans_foci/4-probe/cubic/probe_gnuparallel-*.sh .
-cp ../PolyPhys/bash_pipeline/trans_foci/4-probe/cubic/create_probe_directories.sh .
-cp ../PolyPhys/bash_pipeline/trans_foci/4-probe/cubic/create_probe_directories-next_probes.sh .
+cp ../PolyPhys/bash_pipeline/trans_foci/4-probe/cubic/create_probe_directories-*.sh .
+cp ../PolyPhys/bash_pipeline/trans_foci/4-probe/cubic/create_probe_directories-next_probes-*.sh .
 cp -R ../PolyPhys/polyphys .
