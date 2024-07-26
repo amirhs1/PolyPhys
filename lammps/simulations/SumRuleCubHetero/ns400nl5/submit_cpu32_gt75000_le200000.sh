b70b1e1bf2478f0e4dccec842d14744909cd981e
amirhsi <amirh.sadeghi@icloud.com>
2024-07-26 11:49:47 -0400

    fix mem in submit files

--- lammps/simulations/SumRuleCubHetero/ns400nl5/submit_cpu32_gt75000_le200000.sh
+++ lammps/simulations/SumRuleCubHetero/ns400nl5/submit_cpu32_gt75000_le200000.sh
@@ -5,7 +5,7 @@
 
 #SBATCH --nodes=1 					# number of mpi tasks (cpus); for a serial job, it is the default; 1.
 #SBATCH --ntasks-per-node=32
-#SBATCH --mem=96G                	# memory; default unit is megabytes.
+#SBATCH --mem=3G                	# memory; default unit is megabytes.
 #SBATCH --time=04-10:00             # Total time needed for this job. --time (DD-HH:MM) or -t 0-01:00;  Check the manual of sbatch
 #SBATCH --account=def-byha 		    # The user/account holder who use this computecanada project. you can use -a instead of --acount
 #SBATCH --mail-user=mr.a.h.saadeghi@gmail.com 	# The mail address for notifications about submitted job
